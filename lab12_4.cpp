eb1721cd1822486ca0441d2df6ad402fef24b3ad
Pongmanut <pongmanut_punkam@cmu.ac.th>
2026-01-06 22:08:22 +0700

    Lab12_4 Completed

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
-
 using namespace std;
+
+
+template <typename T>
+void mySwap(T &, T &);
 
 int main(){
 	int x, y;
@@ -29,4 +32,13 @@ int main(){
 
 	return 0;
 }
+
+
+template <typename T>
+void mySwap(T &x, T &y) {
+	T temp = x;
+	x = y;
+	y = temp;
+}
+
 
